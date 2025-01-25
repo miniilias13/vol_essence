2417fa753e760da9b648ef384a4df1d6ea56b2d7
miniilias <iliasagdal88@gmail.com>
2025-01-25 23:13:29 +0100

    Update sv_vol_essence.lua

--- vol_essence/sv_vol_essence.lua
+++ vol_essence/sv_vol_essence.lua
@@ -1,1 +1,11 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+RegisterNetEvent('volerEssence:ajouter')
+AddEventHandler('volerEssence:ajouter', function(amount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer then
+        xPlayer.addInventoryItem('essence', math.floor(amount))
+    end
+end)
