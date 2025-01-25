3f8facd3ae75e5b867a2d2faca44b693a2d2064a
miniilias <iliasagdal88@gmail.com>
2025-01-25 23:18:40 +0100

    Update sv_vol_essence.lua

--- vol_essence/sv_vol_essence.lua
+++ vol_essence/sv_vol_essence.lua
@@ -9,3 +9,16 @@ AddEventHandler('volerEssence:ajouter', function(amount)
         xPlayer.addInventoryItem('essence', math.floor(amount))
     end
 end)
+-- Alerte pour les policiers
+RegisterNetEvent('volerEssence:alertePolice')
+AddEventHandler('volerEssence:alertePolice', function(coords)
+    local xPlayers = ESX.GetPlayers()
+
+    for _, playerId in ipairs(xPlayers) do
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        if xPlayer.job.name == 'police' then
+            TriggerClientEvent('esx:showNotification', playerId, "~r~Une personne est en train de voler de l'essence !")
+            TriggerClientEvent('volerEssence:marqueurPolice', playerId, coords)
+        end
+    end
+end)
